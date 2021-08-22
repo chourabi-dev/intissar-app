5d02bff39c543da321fcd6310432fd98cd74d3e3
chourabi-dev <chourabitahers62@gmail.com>
2021-08-22 13:53:50 +0100

    intisarr

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'intissar';
+  calendar="Semaine: 21.2/05/21 - 30/02/21"
+}
